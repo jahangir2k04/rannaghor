a18c9fc68e2d2e92a770a0d9c0e6ab11ad2fc0ea
jahangir2k04 <jahangir2k04gmail.com>
2023-05-02 22:04:03 +0600

    responsive navigation bar added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Header from '../pages/Shared/Header/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
+import NavBar from '../pages/Shared/NavBar/NavBar';
 
 const Main = () => {
     return (
         <div>
-            <Header></Header>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
