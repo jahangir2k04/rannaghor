7bfc7841d6ab7e7d34166013456932bfc9544e37
jahangir2k04 <jahangir2k04gmail.com>
2023-05-03 14:33:46 +0600

    login page added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVq2VYYdwEjk1ZcnRN9l6af3Lq25mp1Jc",
+  authDomain: "rannaghor-ff202.firebaseapp.com",
+  projectId: "rannaghor-ff202",
+  storageBucket: "rannaghor-ff202.appspot.com",
+  messagingSenderId: "685012722488",
+  appId: "1:685012722488:web:6fe8f9f042e166701e17cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Login/Login/Login.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <div className='mb-10 rounded-lg border-slate-400 w-2/5 mx-auto p-12 shadow-lg'>
+            <h3 className='mb-6 text-4xl font-bold text-center'>Please Login</h3>
+            <form >
+                <div className='text-xl mb-5'>
+                    <label htmlFor="">Email</label> <br />
+                    <input className='mt-2 border rounded border-slate-400 h-12 w-full' type="email" name="email" required />
+                </div>
+                <div className='text-xl mb-5'>
+                    <label htmlFor="">Password</label> <br />
+                    <input className='mt-2 border rounded border-slate-400 h-12 w-full' type="password" name="password" required />
+                </div>
+                <div className='flex mb-8'>
+                    <label className='grow'>
+                        <input type="checkbox" />
+                        <span className='ms-2 text-xl'>
+                            Show password
+                        </span>
+                    </label> 
+                    <Link className='my-primary text-xl font-semibold'>Forgot password?</Link>
+                </div>
+                <input className='w-full my-bg-primary rounded-lg h-12 text-white font-bold text-xl' type="submit" value="Login" />
+            </form>
+            <div className='my-10 text-center grid grid-cols-3 items-center'>
+                 <hr />
+                 <p className='text-xl'>Or continue with</p>
+                 <hr />
+            </div>
+            <div className='flex gap-5 mb-10'>
+                <Link className='flex gap-5 justify-center items-center w-full border border-lime-700 rounded-lg'>
+                    <img className='h-10' src="/src/assets/google.png" alt="" />
+                    <button className='h-12 my-primary font-bold text-xl'>Google</button>
+                </Link>
+                <Link className='flex gap-5 justify-center items-center w-full border border-lime-700 rounded-lg'>
+                    <img className='h-10' src="/src/assets/twitter.png" alt="" />
+                    <button className='h-12 my-primary font-bold text-xl'>Twitter</button>
+                </Link>
+            </div>
+            <p className='text-center text-xl'>
+                Don't Have An Account? <Link to='/register' className='my-primary font-bold'> Register</Link>
+            </p>
+        </div>
+    );
+};
+
+export default Login;
