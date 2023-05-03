cbd6112e2928f681431c927abc55fd42e5d5dc48
jahangir2k04 <jahangir2k04gmail.com>
2023-05-03 11:12:04 +0600

    footer added with responsive

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../pages/Shared/Header/Header';
+import Header from '../pages/Home/Header/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer/Footer';
 import NavBar from '../pages/Shared/NavBar/NavBar';
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <div className="navbar">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <label tabIndex={0} className="ps-0 pe-1 btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 my-primary font-semibold ">
@@ -43,7 +43,7 @@ const NavBar = () => {
                     </div>
                 </label>
                 <Link to='/login'>
-                    <button  className='my-bg-primary my-hover px-6 py-2 text-2xl text-white font-semibold rounded-lg'>Login</button>
+                    <button  className='my-bg-primary my-hover px-3 md:px-6 py-1 md:py-2 text-2xl text-white font-semibold rounded-lg'>Login</button>
                 </Link>
             </div>
         </div>
