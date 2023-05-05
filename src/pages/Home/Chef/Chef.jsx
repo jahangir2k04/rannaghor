544d798755119ace47727efcf8756668e19e89a0
jahangir2k04 <jahangir2k04gmail.com>
2023-05-05 09:40:23 +0600

    banner added in chef recipes page

--- src/pages/Home/Chef/Chef.jsx
+++ src/pages/Home/Chef/Chef.jsx
@@ -4,23 +4,24 @@ import { Link } from 'react-router-dom';
 
 const Chef = ({ chef }) => {
 
-    const { chef_name, experience, chef_pic, likes, recipes } = chef;
+    const { chef_id, chef_name, experience, chef_pic, recipes_num, likes, recipes } = chef;
 
     return (
         <div className='ps-12'>
             <div>
-                <img className='h-64 w-72' src={chef_pic} alt="" />
+                <img className='h-72 w-72' src={chef_pic} alt="" />
             </div>
             <h3 className='my-1 text-2xl font-semibold'>{chef_name}</h3>
             <p className='my-primary font-semibold text-lg'>{experience} Years of experience</p>
-            <p className='text-base w-72 my-2'><span className='font-semibold my-2'>Best Recipes: </span> 
+            <p className='text-base w-72 my-1'><span className='font-semibold my-2'>Best Recipes: </span> 
                 {
                     recipes.map(recipe => recipe.recipe_name).join(' ,  ')
                 }
             </p>
+            <p className='font-semibold mb-1'>Number of Recipes : {recipes_num}+</p>
             <div className='flex gap-2 items-center text-base font-semibold'><span>Likes : {likes}</span> <FaThumbsUp />
             </div>
-            <Link>
+            <Link to={`/chef-recipes/${chef_id}`}>
                 <button className='mt-5 w-72 py-2 my-bg-primary my-hover text-xl text-white font-semibold rounded-lg'>View Recipes</button>
             </Link>
         </div>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,8 @@ import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login/Login";
 import Register from "../pages/Login/Register/Register";
+import ChefRecipes from "../pages/ChefRecipes/ChefRecipes";
+
 
 const router = createBrowserRouter([
     {
@@ -12,6 +14,11 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/chef-recipes/:id',
+                element: <ChefRecipes></ChefRecipes>,
+                loader: ({params}) => fetch(`http://localhost:5000/chef-recipes/${params.id}`)
             },
             {
                 path: 'login',
